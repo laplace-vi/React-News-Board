002caff14934a4d744d9027ee3dcab58a132ccdc
ethan <shuyouxin1@gmail.com>
2016-12-05 10:05:56 +0800

    add webpack

---
+++ client/webpack.production.config.js
@@ -0,0 +1,52 @@
+"use strict";
+
+var webpack = require('webpack');
+var path = require('path');
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+
+var ROOT_PATH = path.resolve(__dirname);
+
+module.exports = {
+  devtool: process.env.NODE_ENV === 'production' ? '' : 'source-map',
+  entry: [
+    path.resolve(ROOT_PATH, './index'),
+  ],
+  module: {
+    loaders: [
+      { test: /\.js?$/, exclude: /node_modules/, loaders: ['babel-loader'] },
+      { test: /\.scss$/, loaders: ['style', 'css', 'sass'] },
+      { test: /\.css?$/, loader: 'style!css' },
+    ],
+  },
+  sassLoader: {
+    includePaths: [
+      path.resolve(ROOT_PATH, './node_modules/compass-mixins/lib'),
+      path.resolve(ROOT_PATH, './node_modules/primer-css/scss')
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  output: {
+    path: path.resolve(ROOT_PATH, 'public'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlwebpackPlugin({
+      template: './index.html'
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+			compress: {
+				warnings: false,
+				screw_ie8: true,
+				drop_console: true,
+				drop_debugger: true
+			}
+		}),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': '"production"'
+    })
+  ]
+};
